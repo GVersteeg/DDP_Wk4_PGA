79c25ac6b6348ee1ca18057553186ac15c3565ce
GVersteeg <gversteeg@fourpoints.nl>
2017-07-30 13:09:16 +0200

    Added a help tab, for the documentation part of the assignment

--- PolutionMonitor/ui.R
+++ PolutionMonitor/ui.R
@@ -11,7 +11,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Air Polution Monitor"),
+  titlePanel("Air Polution Monitor (APM)"),
   
   # Sidebar with two inputs (component & date) 
   sidebarLayout(
@@ -36,7 +36,25 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("dayPlot")
-    )
+        tabsetPanel(type = "tabs",
+             tabPanel("Plot", br(), plotOutput("dayPlot")),
+             tabPanel("Help", br(), 
+                 h3("How to use APM"),
+                 p("The Air Polution Monitor plots the hourly levels
+                   of a specific polutant on an average weekday."),
+                 p("The hourly levels are an average of the polution levels
+                   of measurements taken on three different locations in
+                   the Hague (the Netherlands) during the years 2015-2016."),
+                 p("You can select the polutant you want to see and the weekday
+                   you are interested in. The APM-app will calculate 
+                   the polution levels for each hour during the chosen weekday 
+                   averaged over the years 2015-2016. It will also show a 
+                   smoothed regression line through those points"),
+                 p("The APM will enable you to analyze the daily pattern
+                   for the chosen polutant and weekday. Be aware of the fact 
+                   that some stations do not measure all of the five polutants")
+                 )
+                   )
+              )
   )
 ))
